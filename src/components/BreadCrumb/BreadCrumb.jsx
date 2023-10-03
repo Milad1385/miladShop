7a0e1467977b9a1b12dec925183abcd78d49ba72
milad salamian <miladsalami1385@gmail.com>
2023-10-03 13:24:05 -0700

    fix pro

--- src/components/BreadCrumb/BreadCrumb.jsx
+++ src/components/BreadCrumb/BreadCrumb.jsx
@@ -4,7 +4,7 @@ export default function BreadCrumb(props) {
   return (
     <>
         <div className='breadCrumb bg-white shadow-sm flex items-center p-2.5  gap-x-1 md:gap-x-5 lg:gap-x-10 text-sm md:text-lg px-2 md:px-5 rounded-md md:rounded-xl'>
-          <div className='breadCrumb__item lg:mr-5 bg-gray-100 rounded-md p-2'>
+          <div className='breadCrumb__item lg:mr-5 bg-gray-100 rounded-md p-2 ml-2'>
                 <svg className='w-6 h-6'>
                   <use href='#home'></use>
                 </svg>
