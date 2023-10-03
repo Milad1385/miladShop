881aca85209d493d25b2b22fac55f5075c5cf823
milad salamian <miladsalami1385@gmail.com>
2023-10-03 13:23:06 -0700

    fix pro

--- src/components/BreadCrumb/BreadCrumb.jsx
+++ src/components/BreadCrumb/BreadCrumb.jsx
@@ -3,28 +3,26 @@ import React from 'react'
 export default function BreadCrumb(props) {
   return (
     <>
-        <div className='breadCrumb bg-white shadow-sm flex items-center gap-x-1 md:gap-x-5 lg:gap-x-10 text-sm md:text-lg px-2 md:px-5 rounded-md md:rounded-xl'>
+        <div className='breadCrumb bg-white shadow-sm flex items-center p-2.5  gap-x-1 md:gap-x-5 lg:gap-x-10 text-sm md:text-lg px-2 md:px-5 rounded-md md:rounded-xl'>
           <div className='breadCrumb__item lg:mr-5 bg-gray-100 rounded-md p-2'>
                 <svg className='w-6 h-6'>
                   <use href='#home'></use>
                 </svg>
           </div>
           <span className='hidden md:block w-1.5 h-14 bg-gray-100' ></span>
-          <svg className='w-5 h-5 block md:hidden'>
-            <use href='#chevron-left'></use>
-          </svg>
+          
           <div className='breadCrumb__item font-DanaDemiBold '>
             <a href="#">محصولات</a>
           </div>
           <span className='hidden md:block w-1.5 h-14 bg-gray-100' ></span>
-          <svg className='w-5 h-5 block md:hidden'>
+          <svg className='w-4 h-5 block md:hidden'>
             <use href='#chevron-left'></use>
           </svg>
           <div className='breadCrumb__item font-DanaDemiBold'>
             <a href="#">{props.categoryName}</a>
           </div>
           <span className='hidden md:block w-1.5 h-14 bg-gray-100' ></span>
-          <svg className='w-5 h-5 block md:hidden'>
+          <svg className='w-4 h-5 block md:hidden'>
             <use href='#chevron-left'></use>
           </svg>
           <div className='breadCrumb__item font-DanaMedium '>
